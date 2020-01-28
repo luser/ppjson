ee9adf050b687a69fa6510382234bc8f78831723
Ted Mielczarek <ted@mielczarek.org>
2020-01-27 19:01:28 -0500

    Use serde-transcode to transform on the fly

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
 use json_color::Colorizer;
 use pager::Pager;
+use serde_json::{Serializer, Deserializer};
 use std::env;
 use std::fs::File;
-use std::io::{self, Read, Write};
+use std::io::{self, BufReader, Read, Write};
 use std::process;
 
-fn read_input<R>(reader: &mut R) -> io::Result<String>
+fn handle_input<R>(reader: &mut R) -> io::Result<()>
 where
     R: Read,
 {
-    let mut s = String::new();
-    reader.read_to_string(&mut s)?;
-    Ok(s)
+    // I don't like this but what can you do?
+    colored::control::set_override(true);
+    //TODO: figure out something useful for Windows.
+    Pager::with_pager("less -FRSX").setup();
+    let colorizer = Colorizer::arbitrary();
+    let stdout = io::stdout();
+    let mut lock = stdout.lock();
+    let mut deserializer = Deserializer::from_reader(reader);
+    let mut serializer = Serializer::with_formatter(&mut lock, colorizer);
+    serde_transcode::transcode(&mut deserializer, &mut serializer)?;
+    serializer.into_inner().flush()?;
+    writeln!(&mut lock, "")?;
+    Ok(())
 }
 
 fn work() -> io::Result<()> {
-    let input = match env::args_os().nth(1) {
+    match env::args_os().nth(1) {
         Some(ref arg) if arg == "--help" => {
             print!(
                 "JSON pretty printer
@@ -27,30 +38,19 @@ Pretty print the contents of the JSON file at PATH including colorizing the outp
 and piping it through a pager. If PATH is not provided read a JSON file from stdin.
 "
             );
-            return Ok(());
         }
         Some(ref arg) if arg == "--version" => {
             println!("ppjson {}", env!("CARGO_PKG_VERSION"));
-            return Ok(());
         }
         Some(filename) => {
-            let mut f = File::open(&filename)?;
-            read_input(&mut f)?
+            let mut f = BufReader::new(File::open(&filename)?);
+            handle_input(&mut f)?;
         }
         None => {
             let mut stdin = io::stdin();
-            read_input(&mut stdin)?
+            handle_input(&mut stdin)?;
         }
-    };
-    // I don't like this but what can you do?
-    colored::control::set_override(true);
-    //TODO: figure out something useful for Windows.
-    Pager::with_pager("less -FRSX").setup();
-    let colorizer = Colorizer::arbitrary();
-    let stdout = io::stdout();
-    let mut lock = stdout.lock();
-    colorizer.colorize_to_writer(&input, &mut lock)?;
-    writeln!(&mut lock, "")?;
+    }
     Ok(())
 }
 
